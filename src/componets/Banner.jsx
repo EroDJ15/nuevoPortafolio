57dc432b0731743d1bda2c357ffa59bca4bf2b18
Yerovic Pino <jerlpino@misena.edu.co>
2023-12-08 18:29:52 -0500

    septimo commit nuevo portafolio

--- src/componets/Banner.jsx
+++ src/componets/Banner.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiFillGithub } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/Ai";
 import { BiLogoLinkedin, BiLogoGmail } from "react-icons/Bi";
 import { BsWhatsapp } from "react-icons/Bs";
 
