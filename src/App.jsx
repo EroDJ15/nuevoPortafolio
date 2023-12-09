2faec5a3906ef507e265db941380089ddba9e58a
Yerovic Pino <jerlpino@misena.edu.co>
2023-12-08 21:39:04 -0500

    octavo commit nuevo portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./componets/Navbar";
 import Banner from "./componets/Banner";
 import About from "./componets/About";
+import Contact from "./componets/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Banner />
         <About />
+        <Contact />
       </main>
     </>
   );
